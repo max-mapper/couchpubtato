9ea643c9266861fe4ef471a4249239e1f686ff42
Max Ogden <max@maxogden.com>
2010-10-13 16:54:25 -0700

    descriptive namesgst

--- updates/test.js
+++ updates/test.js
@@ -1,6 +1,6 @@
 function(doc, req){
-  var lib = require('vendor/xmlToActivityStream');
-  var data = lib.xmlToJson(req.body);
+  var lib = require('vendor/xmlToActivityStreamJson');
+  var data = lib.xmlToActivityStreamJson(req.body);
   data['_id'] = req.uuid;
   return [data, "posted"]
 }
