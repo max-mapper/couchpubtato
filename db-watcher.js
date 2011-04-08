9aa8d3463c7f1c553e471289986f98c878f5a7a8
maxogden <max@maxogden.com>
2011-04-07 19:49:46 -0700

    switch to newer dbemitter and xml2js

--- db-watcher.js
+++ db-watcher.js
@@ -1,35 +1,27 @@
-// adapted from https://github.com/szayat/node.couch.js
-
-var stdin = process.openStdin();
-var vm = require('vm');
-var sys = require('sys');
-
+var stdin = process.openStdin(),
+    sys = require('sys'),
+    dbemitter = require('./dbemitter/main'),
+    request = require('request'),
+    child = require('child_process'),
+    path = require('path'),
+    db = process.argv[2],
+    h = {'content-type':'application/json', 'accept':'application/json'},
+    emitter = dbemitter.createCouchDBEmitter(db),
+    children = {};
+    
 stdin.setEncoding('utf8');
-
-var buffer = ''
-  , docs = {}
-  , listenerDoc
-  ;
 
 var spawnFeedProcess = function( doc ) {
   sys.puts( "Starting process for " + doc._id )
-  var p = child.spawn( process.execPath, [ path.join(__dirname, 'feed-watcher.js') ] );
+  var p = child.spawn( process.execPath, [ path.join(__dirname, 'feed-archiver.js') ] );
   p.stderr.on( "data", function ( chunk ) { sys.error( "data error: " + chunk.toString() ) } )
-  p.stdin.write(JSON.stringify(["listenerDoc", listenerDoc])+'\n');
   p.stdin.write(JSON.stringify(["doc", doc])+'\n');
-  docs[ doc._id ]._feed_process = function() { return p };
+  children[ doc._id ] = {'feed_process': function() { return p }};
 }
 
-stdin.on('data', function (chunk) {
-  buffer += chunk.toString();
-  while (buffer.indexOf('\n') !== -1) {
-    line = buffer.slice(0, buffer.indexOf('\n'));
-    buffer = buffer.slice(buffer.indexOf('\n') + 1);  
-    var obj = JSON.parse(line);
-    if ((obj[1]) === "ddoc") {
-      listenerDoc = obj[1];
-    } else if (obj[0] === "doc") {
-      spawnFeedProcess(obj[1]);
-    }
+emitter.on('change', function (change) {
+  var doc = change.doc;
+  if (doc.feed && doc.db) {
+    spawnFeedProcess(doc);
   }
 });
--- feed-archiver.js
+++ feed-archiver.js
@@ -1,46 +1,26 @@
-#!/usr/bin/env node
+var fs = require('fs'),
+    sys = require('sys'),
+    xml2js = require('./node-xml2js/lib/xml2js'),
+    url = require('url'),
+    request = require('request'),
+    http = require('http'),
+    crypto = require('crypto'),
+    couchdb = require('./lib/node-couchdb-min/couchdb'),
+    stdin = process.openStdin(),
+    buffer = '',
+    debug = false;
 
-var fs = require('fs')
-  , sys = require('sys')
-  , xml2js = require('xml2js')
-  , url = require('url')
-  , http = require('http')
-  , crypto = require('crypto')
-  , parser = new xml2js.Parser();
-  
-var debug = false;
-
-exports.processFeed = function(feedUrl, feedSaveFunc) {
-  
+function processFeed(feedUrl, feedSaveFunc) {
   var feed = url.parse(feedUrl);
-
-  var feedClient = http.createClient(80, feed.host);
-
-  var request = feedClient.request("GET", feed.pathname, {'host' : feed.host});
-  request.end();
-
-  request.on('response', function(response) {
-    response.setEncoding('utf8');
-    
-    var data;
-    
-    response.on('data', function(chunk) {
-      data += chunk;
-    });
-    
-    response.addListener('end', function() {
-      parser.parseString(data);
-    })
-    
-  });
-  
-  parser.addListener('end', feedSaveFunc);
+  request({uri:feed.href, headers: {'host' : feed.host}}, function (error, resp, body) {
+    if (error) throw error;
+    var parser = new xml2js.Parser(feedSaveFunc);
+    parser.parseString(body);
+  })
 }
 
-exports.saveItem = function(item, db, uniqueKey) {
-  
+function saveItem(item, db, uniqueKey) {
   var _id = crypto.createHash('md5').update(item[uniqueKey]).digest("hex");
-  
   db.get(_id, function(err, doc) {
     if (err) {
       if (err.couchDbError == "not_found") {
@@ -52,3 +32,27 @@ exports.saveItem = function(item, db, uniqueKey) {
     } 
   });
 }
+
+function fetchFeed (doc) {
+  var starttime = new Date();   
+  function feedSaveFunc(data) {
+    for (var item in data.channel.item) {
+      saveItem(data.channel.item[item], new couchdb.Db(doc.db), "description");
+    }
+  }
+  processFeed(doc.feed, feedSaveFunc);
+  var endtime = new Date();
+  setTimeout(fetchFeed, doc.interval ? doc.interval : (((endtime - starttime) * 5) + 300000));
+}
+
+stdin.on('data', function (chunk) {
+  buffer += chunk.toString();
+  while (buffer.indexOf('\n') !== -1) {
+    line = buffer.slice(0, buffer.indexOf('\n'));
+    buffer = buffer.slice(buffer.indexOf('\n') + 1);  
+    var obj = JSON.parse(line);
+    if (obj[0] === "doc") {
+      fetchFeed(obj[1]);
+    }
+  }
+});
