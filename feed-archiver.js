9a1c2b43222a6d9947a0c3a81369ccb262ceee15
maxogden <max@maxogden.com>
2011-04-07 22:13:09 -0700

    switch to jsdom

--- feed-archiver.js
+++ feed-archiver.js
@@ -1,6 +1,6 @@
 var fs = require('fs'),
     sys = require('sys'),
-    xml2js = require('./node-xml2js/lib/xml2js'),
+    jsdom = require('jsdom'),
     url = require('url'),
     request = require('request'),
     http = require('http'),
@@ -9,22 +9,39 @@ var fs = require('fs'),
     stdin = process.openStdin(),
     buffer = '',
     debug = false;
+        
+    if (process.argv[2]) {
+      fetchFeed({
+       "_id": "fdfb25620af1adcf55083e3fd80155e2",
+       "_rev": "1-1ec61ec9658c6da3bf364018e19f4e37",
+       "feed": "http://www.nytimes.com/services/xml/rss/nyt/pop_top.xml",
+       "db": "articles"
+      })
+    }
 
 function processFeed(feedUrl, feedSaveFunc) {
   var feed = url.parse(feedUrl);
   request({uri:feed.href, headers: {'host' : feed.host}}, function (error, resp, body) {
+    // ??? resp.setEncoding('utf8');
     if (error) throw error;
-    var parser = new xml2js.Parser(feedSaveFunc);
-    parser.parseString(body);
+    jsdom.env(body, ['jquery.js', 'jfeed.js', 'jatom.js', 'jfeeditem.js', 'jrss.js'], function(errors, window) {
+      if (window.$("channel").length > 0) {
+        var jf = new window.JFeed(window.document);
+        feedSaveFunc(jf.items);
+      } else {
+        sys.log(feedUrl + " had no channel entries");
+      }
+    });
   })
 }
 
 function saveItem(item, db, uniqueKey) {
-  var _id = crypto.createHash('md5').update(item[uniqueKey]).digest("hex");
+  var _id = crypto.createHash('md5').update(item[uniqueKey]).digest("hex");  
   db.get(_id, function(err, doc) {
     if (err) {
       if (err.couchDbError == "not_found") {
-        db.put(_id, item, function(err, result) {
+        db.put(_id, {item: item.description}, function(err, result) {
+          sys.log( "Doc " + JSON.stringify({item: item.description}));
           if (err) return sys.error(err.stack);
           sys.log( "Created " + _id);
         });
@@ -36,8 +53,8 @@ function saveItem(item, db, uniqueKey) {
 function fetchFeed (doc) {
   var starttime = new Date();   
   function feedSaveFunc(data) {
-    for (var item in data.channel.item) {
-      saveItem(data.channel.item[item], new couchdb.Db(doc.db), "description");
+    for (var item in data) {
+      saveItem(data[item], new couchdb.Db(doc.db), "description");
     }
   }
   processFeed(doc.feed, feedSaveFunc);
---
+++ jfeed.js
@@ -0,0 +1,39 @@
+/* jFeed : jQuery feed parser plugin
+ * Copyright (C) 2007 Jean-François Hovinne - http://www.hovinne.com/
+ * Dual licensed under the MIT (MIT-license.txt)
+ * and GPL (GPL-license.txt) licenses.
+ */
+(function( window, undefined ) {
+
+var JFeed = function(xml) {
+    if(xml) this.parse(xml);
+};
+
+JFeed.prototype = {
+
+    type: '',
+    version: '',
+    title: '',
+    link: '',
+    description: '',
+    parse: function(xml) {
+        
+        if(jQuery('channel', xml).length == 1) {
+        
+            this.type = 'rss';
+            var feedClass = new JRss(xml);
+
+        } else if(jQuery('feed', xml).length == 1) {
+        
+            this.type = 'atom';
+            var feedClass = new JAtom(xml);
+        }
+        
+        if(feedClass) jQuery.extend(this, feedClass);
+    }
+};
+
+// Expose jQuery to the global object
+window.JFeed = JFeed;
+
+})(window);
