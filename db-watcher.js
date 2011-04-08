55ed78b88aa15d2b7117e648846495d856b96520
maxogden <max@maxogden.com>
2011-04-07 20:27:07 -0700

    show child process stdout

--- db-watcher.js
+++ db-watcher.js
@@ -15,6 +15,7 @@ var spawnFeedProcess = function( doc ) {
   sys.puts( "Starting process for " + doc._id )
   var p = child.spawn( process.execPath, [ path.join(__dirname, 'feed-archiver.js') ] );
   p.stderr.on( "data", function ( chunk ) { sys.error( "data error: " + chunk.toString() ) } )
+  p.stdout.on( "data", function ( chunk ) { sys.error( "child says: " + chunk.toString() ) } )
   p.stdin.write(JSON.stringify(["doc", doc])+'\n');
   children[ doc._id ] = {'feed_process': function() { return p }};
 }
